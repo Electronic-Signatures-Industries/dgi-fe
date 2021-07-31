800d04850d18148b96a819a90ad7036659f3507c
Rogelio Morrell <molekilla@gmail.com>
2021-07-31 07:09:45 -0500

    pushing CUFE

--- src/fe/index.ts
+++ src/fe/index.ts
@@ -1,8 +1,12 @@
 export * from './FEBuilder';
 export * from './ItemSchema';
+export * from './DGenSchema';
+export * from './TypedRFESchema';
 export * from './Totales';
 export * from './TypedRFE';
 export * from './Utils';
 
 
+export * from './CUFE';
+export * from './CAFE';
 export * from './models';
